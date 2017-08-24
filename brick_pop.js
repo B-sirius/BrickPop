d76336809328dd6fa01ba795a6185364bd8ca658
b-sirius <padfoot_07@163.com>
2017-08-24 21:02:50 +0800

    bounce

--- brick_pop.js
+++ brick_pop.js
@@ -5,12 +5,19 @@
         throw new Error(text);
     }
 
+    var collision = function(rect1, rect2) {
+        if (rect1.x < rect2.x + rect2.width &&
+            rect1.x + rect1.width > rect2.x &&
+            rect1.y < rect2.y + rect2.height &&
+            rect1.height + rect1.y > rect2.y) {
+            return true;
+        }
+    }
+
     /*
-    	游戏
+        游戏
      */
-    var Game = function(canvas) {
-        this.canvas = canvas; // 画布实例
-        this.ctx = this.canvas.getContext('2d');
+    var Game = function() {
         this.stageMap = {}; // 游戏场景
         this.currStage = null; // 当前场景
     }
@@ -39,7 +46,7 @@
     }
 
     /*
-    	场景父类
+        场景父类
      */
     var Stage = function(canvas, ctx) {
         this.canvas = canvas;
@@ -51,7 +58,7 @@
     }
 
     /*
-    	游戏主体场景
+        游戏主体场景
      */
     var GameStage = function(canvas, ctx, bar, ballList, brickList) {
         Stage.call(this, canvas, ctx); // 继承父类属性
@@ -65,41 +72,105 @@
     GameStage.prototype.constructor = GameStage;
 
     // 更新场景
-    GameStage.prototype.update = function(ctx) {
-        this._clearCanvas(this.ctx, this.canvas); // 清空画布
-		this._drawCanvas(this.ctx);
+    GameStage.prototype.update = function() {
+        this.updateBrickList(); // 更新砖块
+        this.updateBallList(); // 更新球
+
+        this._clearCanvas(); // 清空画布
+        this._drawCanvas();
+
+        var _self = this;
+        requestAnimationFrame(this.update.bind(_self));
     }
 
-    GameStage.prototype._drawCanvas = function(ctx) {
+    GameStage.prototype._drawCanvas = function() {
         // 绘制图形
         var _self = this;
-        _self._drawRect(_self.ctx, _self.bar);
+        _self._drawRect(_self.bar);
         this.brickList.forEach(function(item) {
-            _self._drawRect(_self.ctx, item);
+            _self._drawRect(item);
         });
         this.ballList.forEach(function(item) {
-            _self._drawRect(_self.ctx, item);
+            _self._drawRect(item);
         });
     }
 
     // 绘制方块儿
-    GameStage.prototype._drawRect = function(ctx, rect) {
+    GameStage.prototype._drawRect = function(rect) {
         // 是否填充图形
         if (rect.fill === true) {
-            ctx.fillStyle = rect.color;
-            ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+            this.ctx.fillStyle = rect.color;
+            this.ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
         }
-        ctx.strokeStyle = rect.color;
-        ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+        this.ctx.strokeStyle = rect.color;
+        this.ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
     }
 
     // 清空画布
-    GameStage.prototype._clearCanvas = function(ctx, canvas) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    GameStage.prototype._clearCanvas = function() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    GameStage.prototype.updateBallList = function() {
+        for (var i = this.ballList.length - 1; i >= 0; i--) {
+            var ball = this.ballList[i];
+
+            ball.oldX = ball.x;
+            ball.oldY = ball.y;
+
+            ball.x += ball.dx;
+            ball.y += ball.dy;
+
+            this._brickCollision(ball); // 砖块碰撞检测
+            this._wallCollision(ball); // 边界检测
+        }
+    }
+
+    GameStage.prototype._brickCollision = function(ball) {
+        for (var i = this.brickList.length - 1; i >= 0; i--) {
+            var brick = this.brickList[i];
+            // 是否相碰
+            if (collision(ball, brick)) {
+                brick.impact(); // 砖块被撞击
+                ball.bounce(brick); // 球反弹
+            }
+        }
+    }
+
+    GameStage.prototype._wallCollision = function(ball) {
+        if (ball.x < 0 || ball.x + ball.width > this.canvas.width) {
+            ball.dx = -ball.dx;
+            if (ball.x < 0) {
+                ball.x = 0;
+            } else {
+                ball.x = this.canvas.width - ball.width;
+            }
+        }
+
+        if (ball.y < 0 || ball.y + ball.height > this.canvas.height) {
+            ball.dy = -ball.dy;
+            if (ball.y < 0) {
+                ball.y = 0
+            } else {
+                ball.y = this.canvas.height - ball.height;
+            }
+        }
+    }
+
+    GameStage.prototype.updateBrickList = function() {
+        for (var i = this.brickList.length - 1; i >= 0; i--) {
+            var brick = this.brickList[i];
+
+            // 若砖块hp为0，移除
+            if (brick.hp <= 0) {
+                this.brickList.splice(i, 1);
+                break;
+            }
+        }
     }
 
     /*
-    	方块
+        方块
      */
     var Rect = function(width, height, x, y) {
         this.width = width;
@@ -111,7 +182,7 @@
     }
 
     /*
-    	挡板
+        挡板
      */
     var Bar = function(width, height, x, y) {
         Rect.call(this, width, height, x, y);
@@ -121,48 +192,92 @@
     Bar.prototype.constructor = Bar;
 
     /*
-    	砖块
+        砖块
      */
     var Brick = function(width, height, x, y) {
         Rect.call(this, width, height, x, y);
+        this.hp = 1;
     }
 
     Brick.prototype = Object.create(Rect);
     Brick.prototype.constructor = Brick;
 
+    // 砖块被撞到
+    Brick.prototype.impact = function() {
+        --this.hp;
+    }
+
     /*
-    	弹球
+        弹球
      */
     var Ball = function(width, height, x, y) {
         Rect.call(this, width, height, x, y);
+        this.speed = 500 / 60; // 速度
+        this.angle = Math.PI / 4; // 角度
+        this.dx = this.speed * Math.cos(this.angle); // x轴分速度
+        this.dy = this.speed * Math.sin(this.angle); // y轴分速度
+        this.oldX = null; // 上一帧的x
+        this.oldY = null; // 上一帧的y
     }
 
     Ball.prototype = Object.create(Rect);
     Ball.prototype.constructor = Ball;
 
+    // 球反弹
+    Ball.prototype.bounce = function(brick) {
+        // y轴方向的撞击
+        if (this.oldX < brick.x + brick.width &&
+            this.oldX + this.width > brick.x) {
+            this.dy = -this.dy;
+            // 从上方撞击
+            if (this.oldY + this.height < brick.y) {
+                this.y = brick.y - this.height;
+            } else {
+                // 从下方撞击
+                this.y = brick.y + brick.height;
+            }
+        } else {
+            // x轴方向的撞击
+            this.dx = -this.dx;
+            // 从左侧撞击
+            if (this.oldX + this.width < brick.x) {
+                this.x = brick.x - this.width;
+            } else {
+                // 从右侧撞击
+                this.x = brick.x + brick.width;
+            }
+        }
+    }
+
     /*
-    	demo版游戏
+        demo版游戏
      */
     var gameDemo = (function() {
-    	var canvas = document.querySelector('#canvas');
-    	var gameDemo = new Game(canvas)
+        var canvas = document.querySelector('#canvas');
+        var ctx = canvas.getContext('2d');
 
+        var gameDemo = new Game();
 
-    	var bar = new Bar(100, 20, 450, 500);
-    	var ball = new Ball(20, 20, 450, 300);
-    	var ballList = [];
-    	ballList.push(ball);
-    	var brick = new Brick(70, 40, 200, 100);
-    	var brickList = [];
-    	brickList.push(brick);
+        var bar = new Bar(100, 20, 450, 500);
+        var ball = new Ball(10, 10, 450, 400);
+        var ballList = [];
+        ballList.push(ball);
+        
+        var brickList = [];
+        for (var i = 5; i >= 0; i--) {
+            for (var j = 3; j >= 0; j--) {
+                var brick = new Brick(100, 40, i * 100, j * 80 + 50);
+                brickList.push(brick);
+            }
+        }
 
-    	var gameStage = new GameStage(gameDemo.canvas, gameDemo.ctx, bar, ballList, brickList);
-    	var stageMap = {
-    		gameStage: gameStage
-    	};
+        var gameStage = new GameStage(canvas, ctx, bar, ballList, brickList);
+        var stageMap = {
+            gameStage: gameStage
+        };
 
-    	gameDemo.addStage(stageMap);
-    	gameDemo.setStage('gameStage');
-    	gameDemo.start();
+        gameDemo.addStage(stageMap);
+        gameDemo.setStage('gameStage');
+        gameDemo.start();
     })();
 })();
